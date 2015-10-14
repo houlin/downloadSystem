312c4463e6ca7bae87af523231659c6b79a0b7c8
houlin <499389897@qq.com>
2015-10-14 13:24:42 +0800

    Signed-off-by: houlin <499389897@qq.com>

--- DownloadConst.java
+++ DownloadConst.java
@@ -2,6 +2,7 @@ package com.jams.music.player.DownLoadSystem;
 
 /**
  * Created by pc on 2015/10/13.
+ * author by houlin
  */
 public class DownloadConst {
     public static final String  TAG_NAME_DOWNLOAD_SERVICE = "com.jams.music.player.IDownloadService" ;
